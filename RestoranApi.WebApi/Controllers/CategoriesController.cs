4710de41117ce28a6eb5c37244b2df6665839fb1
mustafalmazz <mustafagfb19077091@gmail.com>
2026-04-04 22:08:54 +0300

    feat : ChefController Created and CRUD Operations Completed

---
+++ RestoranApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestoranApi.WebApi.Context;
+using RestoranApi.WebApi.Entities;
+
+namespace RestoranApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            if (_context.Categories.ToList() != null)
+            {
+                return Ok(_context.Categories.ToList());
+            }
+            return NotFound("Kategoriler bulunamadı");
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+    }
+}
---
+++ RestoranApi.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestoranApi.WebApi.Context;
+using RestoranApi.WebApi.Entities;
+
+namespace RestoranApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteChef(int id)
+        {
+            var find = _context.Chefs.Find(id);
+            _context.Chefs.Remove(find);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetChef(int id)
+        {
+            var find = _context.Chefs.Find(id);
+            return Ok(find);
+        }
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            var find = _context.Chefs.Find(chef.ChefId);
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+
+    }
+}
