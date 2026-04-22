4029e95f60d9356617bf52962f17076f6e7dacde
mustafalmazz <mustafagfb19077091@gmail.com>
2026-04-22 21:34:27 +0300

    AutoMapping Operations Added

---
+++ RestoranApi.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestoranApi.WebApi.Context;
+using RestoranApi.WebApi.Dtos.FeatureDtos;
+using RestoranApi.WebApi.Entities;
+
+namespace RestoranApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        IMapper _mapper;
+        private readonly  ApiContext _context;
+        public FeaturesController(IMapper mapper, ApiContext context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+
+            return Ok(_mapper.Map<List<ResultFeatureDto>>(values));
+           
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            _context.Features.Add(value);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFeature(int id)
+        {
+            var find = _context.Features.Find(id);
+            _context.Features.Remove(find);
+            _context.SaveChanges();
+            return Ok("İşlem Başarılı");
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetFeature(int id)
+        {
+            var find = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIdFeature>(find));
+        }
+    }
+}
---
+++ RestoranApi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RestoranApi.WebApi.Dtos.FeatureDtos;
+using RestoranApi.WebApi.Entities;
+
+namespace RestoranApi.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature,CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature,UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature,GetByIdFeature>().ReverseMap();
+        }
+    }
+}
