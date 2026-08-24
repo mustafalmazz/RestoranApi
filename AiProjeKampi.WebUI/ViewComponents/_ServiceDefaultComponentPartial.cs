2e794b577df0e79f4f2a80e26dd6febf66386974
mustafalmazz <mustafagfb19077091@gmail.com>
2026-08-24 14:36:07 +0300

    Add services API and dynamic homepage
    This change adds a Services API with CRUD endpoints and enables the UI to fetch service data from the API using HttpClient. The service view component now renders real service cards dynamically, replacing the static lorem ipsum content. It also includes a Turkish brand description for the Yummy section and new ASP.NET launch settings for local development.

--- AiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ AiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AiProjeKampi.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AiProjeKampi.WebUI.ViewComponents
 {
     public class _ServiceDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7121/api/Services");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ RestoranApi.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestoranApi.WebApi.Context;
+using RestoranApi.WebApi.Entities;
+
+namespace RestoranApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ServicesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var list = _context.Services.ToList();
+            return Ok(list);
+        }
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteService(int id)
+        {
+            var find = _context.Services.Find(id);
+            if (find == null)
+            {
+                return NotFound("Silinecek servis bulunamadı");
+            }
+            _context.Services.Remove(find);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
